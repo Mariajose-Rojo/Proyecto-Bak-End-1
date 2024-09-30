5ac17958213c646b58e85d30a5220009c7f6cefb
Maria Jose Rojo <99.rojomaria@gmail.com>
2024-09-29 22:29:53 -0300

    Modificaciones al server/app.js para que se adapte a sweetAlert

--- src/server.js
+++ src/server.js
@@ -1,57 +1,72 @@
 import express from 'express';
-import productsRouter from './routes/products.js';
-import cartsRouter from './routes/carts.js';
-import { createServer } from 'http';
 import { Server } from 'socket.io';
-import { engine } from 'express-handlebars';
+import handlebars from 'express-handlebars';
 import path from 'path';
+import fs from 'fs/promises';
+import { fileURLToPath } from 'url';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 8080;
 
-//Handlebars
-app.engine('handlebars', engine());
+//configuracion de handlebars
+app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 
-//servidor HTTP junto cxon Socket.IO
-const httpServer = createServer(app);
+app.use(express.static('public'));
+app.use(express.json());
+
+// almacenar los productos en memoria
+let products = [];
+
+// configuarar http y Websockets
+const httpServer = app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
+
 const io = new Server(httpServer);
 
-// Middleware
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Rutas a usar 
-app.use('/api/products', productsRouter);
-app.use('/api/carts', cartsRouter);
-
-// Rutas para las vistas
-app.get('/', (req, res) => {
-  res.render('home'); // Renderiza la vista home.handlebars
+// ruta vista home con los productos
+app.get('/home', (req, res) => {
+  res.render('home', { products });
 });
 
+// ruta vista de productos en tiempo real
 app.get('/realtimeproducts', (req, res) => {
-  res.render('realTimeProducts'); // Renderiza la vista realTimeProducts.handlebars
+  res.render('realTimeProducts', { products });
 });
 
-let products = []; // alamacena la lista de productos
-
+// websockets
 io.on('connection', (socket) => {
   console.log('Nuevo cliente conectado');
 
-  // envia los productos a los clientes
+  // nenviar la lista inicial de productos al conectarse
   socket.emit('updateProducts', products);
 
-  // creacuion de nuevos productos
   socket.on('newProduct', (product) => {
     products.push(product);
-    io.emit('updateProducts', products); // actualizacion de los datos de los clientes
-  });
 
+    // emite el evento a todos los clientes para actualizar la lista de productos
+    io.emit('updateProducts', products);
+
+    // emite la alerta despues de que se agrega el producto
+    io.emit('productAdded', product);
+  });
 });
 
-// Iniciar el servidor
-httpServer.listen(port, () => {
-  console.log(`Listening on PORT ${port}`);
+// Ruta POST, agrega producto y emite websockstes
+app.post('/productos', (req, res) => {
+  const newProduct = req.body;
+
+  products.push(newProduct);
+
+  // emite el evento a través de WebSockets
+  io.emit('updateProducts', products);
+  io.emit('productAdded', newProduct);
+
+  res.json({ message: 'Producto agregado con éxito', product: newProduct });
 });
